85d340e80a679a38ecdafd079c351f7a01e76afb
joi <joi@chromium.org>
2012-11-20 11:06:02 +0000

    Remove shbang line from PRESUBMIT.py.
    This make checkperms happy. PRESUBMIT.py has no main
    function so it doesn't make sense to have a #!.
    
    Patch contributed by sbc@chromium.org
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/11418086

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
@@ -17,6 +16,7 @@ def RunUnittests(input_api, output_api):
 
 def CheckChangeOnUpload(input_api, output_api):
   return RunUnittests(input_api, output_api)
+
 
 def CheckChangeOnCommit(input_api, output_api):
   return RunUnittests(input_api, output_api)
