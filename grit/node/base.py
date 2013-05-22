3169a46150b06e7e1ec1ca3bb8bbb6343baf29f9
joi <joi@chromium.org>
2013-05-22 14:45:50 +0000

    Add is_ios to the list of grit variables
    Adds is_ios mirroring the other platforms, and makes is_macosx false
    and is_posix true for iOS (matching the behavior in gyp).
    
    BUG=https://code.google.com/p/grit-i18n/issues/detail?id=10
    
    Originally by stuartmorgan@chromium.org, https://codereview.chromium.org/15730002/
    
    Review URL: https://codereview.chromium.org/15747003

--- grit/node/base.py
+++ grit/node/base.py
@@ -459,8 +459,9 @@ class Node(object):
         'is_macosx': target_platform == 'darwin',
         'is_win': target_platform in ('cygwin', 'win32'),
         'is_android': target_platform == 'android',
+        'is_ios': target_platform == 'ios',
         'is_posix': (target_platform in ('darwin', 'linux2', 'linux3', 'sunos5',
-                                         'android')
+                                         'android', 'ios')
                     or 'bsd' in target_platform),
         'pp_ifdef' : pp_ifdef,
         'pp_if' : pp_if,
